e0e6e0ffc5c6ec0581de92a13f4ff55c55b53824
amclennan150 <amclennana@storans.school.nz>
2019-10-17 19:03:39 +1300

    Getting the user to inout the starting weight for their pet

--- print_food.py
+++ print_food.py
@@ -3,6 +3,5 @@ FOOD_DICT = {"Carrot": 0.2, "Grass": 0.4, "Celery": 0.1, "Spinach": 0.3}
 number = 1
 
 for food in FOOD_DICT:
-        print("{}.  {} - {} point/s".format(number, food.title(), FOOD_DICT.get(food)))
-        number += 1
-
+    print("{}.  {} - {} point/s".format(number, food.title(), FOOD_DICT.get(food)))
+    number += 1
