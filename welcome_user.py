bd341222b75111a23b3953c5197b055e021048da
amclennan150 <amclennana@storans.school.nz>
2019-10-18 12:05:24 +1300

    creating a formatting function Welcoming the user to the program along and how the game works

---
+++ welcome_user.py
@@ -0,0 +1,14 @@
+# welcome user to program
+
+
+# function for formatting the title
+def formatter(character, output):
+    print(character * (len(output) + 5))
+    print("{} {} {}".format(character, output, character))
+    print(character * (len(output) + 5))
+
+
+# print welcoming message along with how the game works
+print("Welcome to your own virtual pet!\n"
+      "You will be able to choose your pets name along with their beginning weight\n"
+      "For your pet to survive it must be kept between a weight of 1.5kg and 2.5kg, don't worry we'll tell you if your pet is near these")
