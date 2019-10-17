a991dc4c12637f5bf3921890087e411908d852d6
amclennan150 <amclennana@storans.school.nz>
2019-10-17 18:21:00 +1300

    getting the user to insert the name for their pet and setting the starting weight

---
+++ weight_and_name_input.py
@@ -0,0 +1,31 @@
+# allows user to enter the starting weight of their Virtual Pet and name pet
+
+# function to check the user has inputted a float
+
+def check_float(question, error, low, high):  # Question and error allow it to be used for various questions
+    valid = False
+
+    while not valid:
+        number = input("{} >> ".format(question))  # Any question that needs a number as the input
+        try:
+            number = float(number)
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+# Getting the user to name their pet
+name = input("What would you like to name your pet? >> ")
+
+# Getting the user to input their virtual pets starting weight by running through the function
+initial_weight = check_float("Enter the weight of your virtual pet to beginning with (between 1.5 and 2.5) ",
+                             "Whoops please enter a number between 1.5 and 2.5", 1.5, 2.5)
+# print(initial_weight)
+
+# Printing the users input
+print("The beginning weight of {} is {}.".format(name, initial_weight))
+
+
