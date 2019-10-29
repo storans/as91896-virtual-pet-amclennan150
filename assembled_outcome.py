8ad0bbe96acd4258712fcd17464b0b044c8e5559
amclennan150 <amclennana@storans.school.nz>
2019-10-29 17:02:40 +1300

    putting all of the components together for the final outcome. getting the pet to die and the game to end v2

--- assembled_outcome.py
+++ assembled_outcome.py
@@ -73,14 +73,14 @@ def user_choice_food(weight):
         number += 1
         food_menu.append(food)
 
-    choice = check_int("Please choose an option from the list - type the number >>",
-                       "Please choose a number between 1 and 5.", 1, 5)
+    choice = check_int("Please choose an option from the list - type the "
+                       "number >>", "Please choose a number between 1 and 5.",
+                       1, 5)
 
     chosen_food = food_menu[choice - 1]
 
     print("You will feed your pet {}.".format(chosen_food.title()))
 
-    # change_weight(pet_weight, chosen_food)
     weight += FOOD_DICT[chosen_food]
 
     return weight
@@ -110,61 +110,73 @@ def user_choice_exercise(weight):
     return weight
 
 
-def death_checker(weight, pet_alive):
-    if 1.4 < weight > 2.6:
-        print("You must set a goal before entering your points or showing statistics.\n"
-              "Choose Option 2 from the main menu to set your goal.")
-    elif weight >= 2.6:
+def death_checker(weight):
+    if weight > 2.6:
         print("You fed your pet too much and they died")
+        print(weight)
+        pet_alive = False
+
+    elif weight < 1.4:
+        print("You didn't feed your pet enough and they died.")
+        print(weight)
         pet_alive = False
 
     else:
-        print("You didn't feed your pet enough and they died.")
-
-    return weight
+        pet_alive = True
 
 
-# menu for the user to choose an option
-if __name__ == "__main__":
-
-    FOOD_DICT = {"Carrots": 0.2, "Grass": 0.4, "Celery": 0.1, "Spinach": 0.3, "Chocolate": 0.4}
-    EXERCISE_DICT = {"Hop": -0.2, "Walk": -0.4, "Run": -0.1, "Jump": -0.3}
-
-    pet_weight = 0
-    alive = True
-    while alive:
-
-        # Start an infinite loop to prompt for a correct option value
-        while True:
-            # Create an option variable to store the user's chosen option.
-            print()
-            print("-" * 20)
-            print()
-            print("Please choose an option from the following menu:")
-            print("1. Show current weight\n"
-                  "2. Feed Pet\n"
-                  "3. Exercise Pet\n"
-                  "4. See help information\n"
-                  "5. Exit\n")
-            option = check_int("Enter your choice: ", "You must choose a number between 1 and 5.", 1, 5)
-
-            if option == 1:
-                print(pet_weight)
-
-            if option == 2:
-                print()
-                pet_weight = user_choice_food(pet_weight)
+    return pet_alive
 
 
-            if option == 3:
-                print()
-                pet_weight = user_choice_exercise(pet_weight)
+FOOD_DICT = {"Carrots": 0.2, "Grass": 0.4, "Celery": 0.1, "Spinach": 0.3, "Chocolate": 0.4}
+EXERCISE_DICT = {"Hop": -0.2, "Walk": -0.4, "Run": -0.1, "Jump": -0.3}
 
-            if option == 4:
-                help_info()
+# Getting the user to input their virtual pets starting weight by running
+# through the function
+pet_weight = check_float("Enter the weight of your virtual pet to "
+                         "beginning with (between 1.5 and 2.5) ",
+                         "Whoops please enter a number between 1.5 "
+                         "and 2.5", 1.5, 2.5)
+# Setting alive loop to true
 
-            if option == 5:
-                keep_going = False
-                break
+alive = True
 
-    print("Thanks for playing")
+# while the pet is alive
+while alive:
+
+    # menu for the user to choose an option
+
+    print()
+    print("-" * 20)
+    print()
+    print("Please choose an option from the following menu:")
+    print("1. Show current weight\n"
+          "2. Feed Pet\n"
+          "3. Exercise Pet\n"
+          "4. See help information\n"
+          "5. Exit\n")
+    option = check_int("Enter your choice: ", "You must choose a number "
+                                              "between 1 and 5.", 1, 5)
+
+    if option == 1:
+        print(pet_weight)
+
+    if option == 2:
+        print()
+        pet_weight = user_choice_food(pet_weight)
+        alive = death_checker(pet_weight)
+
+
+    if option == 3:
+        print()
+        pet_weight = user_choice_exercise(pet_weight)
+        alive = death_checker(pet_weight)
+
+    if option == 4:
+        help_info()
+
+    if option == 5:
+        alive = False
+        break
+
+print("Thanks for playing")
