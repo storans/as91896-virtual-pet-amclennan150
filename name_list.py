f949587cb48c0915ae4f7d277ba1e95b3f0bbe42
amclennan150 <amclennana@storans.school.nz>
2019-11-04 12:08:44 +1300

    getting the user to choose their pets name from a list of already decided names to be turned into function in assembled outcome

--- name_list.py
+++ name_list.py
@@ -13,43 +13,37 @@ def check_int(question, error, low, high):  # Question and error allow it to be 
             if low <= number <= high:
                 return number
             else:
-                print(error)
+                print(error)  # if the user enters un invalid number
         except ValueError:
             print(error)
-
-
-# function for the user to chooses to name their pet one of the names
-# from the list
-def chose_name():
-    name_option = []
-    print("Choose one of the following names:")
-    number = 1
-    # list foods in a for loop
-    for names in NAME_LIST:
-        # print the names in the list
-        print("{}.  {}".format(number, names.title()))
-        number += 1
-        name_option.append(names)
-
-    # getting the user to enter the number that corresponds to the name they want to pick
-    chosen_name = check_int("Please choose an option from the list - type the "
-                            "number >>", "Please choose a number between 1 and 5.",
-                            1, 5)
-
-    # so the name of the pet will print on the next line
-    name_pet = name_option[chosen_name - 1]
-
-    # printing the chosen name
-    print("Your pets name is {}".format(name_pet))
-
-    # return pets name so can be accessed later
-    return name_pet
 
 
 # list of names for the user to choose from
 NAME_LIST = ["Coco", "Charlie", "Alvin", "Cupcake", "Felix", "Jazz"]
 
+# getting the user to choose their pets name from a list
+# will be function in assembled outcome
 
-# running the function for the user to choose their pets name from a list
-chose_name()
+name_option = []
+print("Choose one of the following names:")
+number = 1
+# list foods in a for loop
+for names in NAME_LIST:
+    # print the names in the list
+    print("{}.  {}".format(number, names.title()))
+    number += 1
+    name_option.append(names)
+
+# getting the user to enter the number that corresponds to the name they want to pick
+chosen_name = check_int("Please choose an option from the list - type the "
+                        "number >>", "Please choose a number between 1 and 5.",
+                        1, 5)
+
+# so the name of the pet will print on the next line
+name_pet = name_option[chosen_name - 1]
+
+# printing the chosen name
+print("Your pets name is {}".format(name_pet))
+
+
 
