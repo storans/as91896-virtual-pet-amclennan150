e4a928aa3b0d6423bf862f08311e2ef215ac440e
amclennan150 <amclennana@storans.school.nz>
2019-11-04 11:03:00 +1300

    print the list of available exercises that the user an choose from

---
+++ print_exercise_dict.py
@@ -0,0 +1,19 @@
+# printing the exercises in the exercise dictionary to
+
+# exercise dictionary with the available exercises to do
+# along with the weight the pet will lose
+EXERCISE_DICT = {"Hop": -0.2, "Walk": -0.4, "Run": -0.1, "Jump": -0.3}
+
+# setting number to one so the foods will print with the correct number
+number = 1
+# show which sports are available to be chosen
+for activity in EXERCISE_DICT:
+    print("{}.  {}".format(number, activity.title()))
+    number += 1
+
+
+
+
+
+
+
