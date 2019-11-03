8b27f0c25a5a4ab975bb02ddcde7e95559a55cb3
amclennan150 <amclennana@storans.school.nz>
2019-11-04 12:03:00 +1300

    print the list of available exercises the user can choose from  (dictionary)

--- print_food_dict.py
+++ print_food_dict.py
@@ -3,7 +3,7 @@
 # food dictionary with values of weight to be added
 FOOD_DICT = {"Carrot": 0.2, "Grass": 0.4, "Celery": 0.1, "Spinach": 0.3}
 
-# setting number to one so the foods will print with the correct nUmber
+# setting number to one so the foods will print with the correct number
 number = 1
 
 # printing the food dictionary in a list with numbers
