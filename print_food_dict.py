2ef26389bad8b5d00fcac8edbb1c711548292074
amclennan150 <amclennana@storans.school.nz>
2019-11-04 08:51:02 +1300

    printing the food in the food dictionary  food dictionary with values of weight to be added  setting number to one so the foods will print with the correct number  printing the food dictionary in a list with numbers

---
+++ print_food_dict.py
@@ -0,0 +1,12 @@
+# printing the food in the food dictionary
+
+# food dictionary with values of weight to be added
+FOOD_DICT = {"Carrot": 0.2, "Grass": 0.4, "Celery": 0.1, "Spinach": 0.3}
+
+# setting number to one so the foods will print with the correct nUmber
+number = 1
+
+# printing the food dictionary in a list with numbers
+for food in FOOD_DICT:
+    print("{}.  {}".format(number, food.title()))
+    number += 1
