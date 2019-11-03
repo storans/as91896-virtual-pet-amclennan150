9ae4bd391a86540241e87f1e954be66c37d72301
amclennan150 <amclennana@storans.school.nz>
2019-11-04 12:10:18 +1300

    printing the exercises available for the user to do to the pet with corresponding numbers, exercise dictionary with the available exercises to do along with the weight the pet will lose

--- exercise_list.py
+++ exercise_list.py
@@ -1,18 +1,16 @@
 # printing the exercises available for the user to do to the pet
 # with corresponding numbers
 
-
-# function to print the exercises available
-def show_exercise():
-    number = 1
-    # print the exercises in the list
-    for activity in EXERCISE_DICT:
-        print("{}.  {}".format(number, activity.title()))
-        number += 1
-
 # exercise dictionary with the available exercises to do
 # along with the weight the pet will lose
 EXERCISE_DICT = {"Hop": -0.2, "Walk": -0.4, "Run": -0.1, "Jump": -0.3}
 
-# run function
-show_exercise()
+number = 1
+# print the exercises in the list
+for activity in EXERCISE_DICT:
+    print("{}.  {}".format(number, activity.title()))
+    number += 1
+
+
+
+
